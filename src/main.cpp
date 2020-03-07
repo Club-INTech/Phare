0e60ae7490d950a9c0cf6a9ab7db2da96e44e03f
LamiaLaBg <lamia.salhi@telecom-sudparis.eu>
2020-03-07 22:47:44 +0100

    création de la classe main et séparation pour les pdifférentes parties du code

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by SALHI Lamia et Tahar et raphi la malice et renax le renard on 07/03/2020.
+//
+
+#include <Arduino.h>
+
+void setup(){
+// bouton poussoir
+// Neopixel
+// code XL
+}
+void loop(){
+// bouton poussoir
+// Neopixel
+// code XL
+}
