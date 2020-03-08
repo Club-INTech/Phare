a9d38c8781ada191288910a1d55126057d2c816d
remi.lamiaux <remi.lamiaux@telecom-sudparis.eu>
2020-03-09 00:20:44 +0100

    nouvelle lib softwareserial pour esp32

--- lib/Dynamixel-Com/src/DynamixelManager.h
+++ lib/Dynamixel-Com/src/DynamixelManager.h
@@ -11,12 +11,13 @@
 #include "DynamixelPacketSender.h"
 #include "DynamixelMotor.h"
 #include <map>
-#ifndef LED_BUILTIN
+
+#if defined(ESP32) && !defined (LED_BUILTIN) //LED_BUILTIN n'est pas définie sur ESP32
 #define LED_BUILTIN 2
 #endif
 // TODO : Rajouter les vraies fonctions de manager
 
-typedef DynamixelMotor* MotorGeneratorFunctionType(uint8_t, DynamixelPacketSender*);
+typedef DynamixelMotor *MotorGeneratorFunctionType(uint8_t, DynamixelPacketSender *);
 //!High-level DynamixelMotor interface
 /*!
  * The first goal of the DynamixelManager is to provide a way for every DynamixelMotor to communicate.
@@ -25,25 +26,27 @@ typedef DynamixelMotor* MotorGeneratorFunctionType(uint8_t, DynamixelPacketSende
  * \li Motor instantiation and ID conflict prevention
  * \li Motor control by ID
  */
-class DynamixelManager: public DynamixelPacketSender {
+class DynamixelManager : public DynamixelPacketSender {
 
 public:
     /**
      * Dumb constructor, common to the other ones.
      * @param debugSerial Serial to output debug messages.
      */
-    explicit DynamixelManager(Stream* debugSerial);
+    explicit DynamixelManager(Stream *debugSerial);
+
     /**
      * Constructs a new DynamixelManager with the serial used for communication with Dynamixel motors and one for debugging that must have begun communication (with begin() ) (can be left to NULL if not needed)
      */
-    explicit DynamixelManager(HardwareSerial*, Stream*  = nullptr, long baudrate = 57600);
+    explicit DynamixelManager(HardwareSerial *, Stream * = nullptr, long baudrate = 57600);
+
     /**
      * Constructs a new DynamixelManager and a new SoftwareSerial on the designated pins.
      * @param RX Receive pin, board side
      * @param TX Transmit pin, board side
      * @param baudrate
      */
-    DynamixelManager(int RX, int TX, Stream*  = nullptr, long baudrate = 57600);
+    DynamixelManager(int RX, int TX, Stream * = nullptr, long baudrate = 57600);
 
 
     /*!
@@ -53,38 +56,37 @@ public:
      * @param packet
      * @return Response string, eventually empty.
      */
-    char* sendPacket(DynamixelPacketData *) const override;
+    char *sendPacket(DynamixelPacketData *) const override;
 
     /*!
      * Reads a single DynamixelPacket from the serial port.
      * @param responseSize the expected packet size
      * @return The packet string.
      */
-    char* readPacket(uint8_t responseSize) const override;
+    char *readPacket(uint8_t responseSize) const override;
 
     /*!
      * Creates a motor instance based on the given function, registered with the given ID
      * @return a new motor instance
      */
-    DynamixelMotor* createMotor(uint8_t, MotorGeneratorFunctionType);
+    DynamixelMotor *createMotor(uint8_t, MotorGeneratorFunctionType);
 
     /*!
      * Get a motor instance based on the given ID
      */
-     DynamixelMotor* getMotor(uint8_t);
+    DynamixelMotor *getMotor(uint8_t);
 
 private:
-    Stream* serial;
+    Stream *serial;
 
-    std::map<uint8_t, DynamixelMotor*> motorMap;
+    std::map<uint8_t, DynamixelMotor *> motorMap;
 
-    Stream* debugSerial;
+    Stream *debugSerial;
 
     int TX;
     int RX;
 
-    void setHalfDuplex(const Stream & mStream) const
-    {
+    void setHalfDuplex(const Stream &mStream) const {
 #if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) // Teensy 3.0 3.1 3.2 3.5 3.6
         if (&mStream == &Serial1)
         {
@@ -121,8 +123,7 @@ private:
 #endif
     }
 
-    void setReadMode(const Stream & mStream) const
-    {
+    void setReadMode(const Stream &mStream) const {
 #if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) // Teensy 3.0 3.1 3.2 3.5 3.6
         if (&mStream == &Serial1)
         {
@@ -159,8 +160,7 @@ private:
     }
 
 
-    void setWriteMode(const Stream & mStream) const
-    {
+    void setWriteMode(const Stream &mStream) const {
 #if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) // Teensy 3.0 3.1 3.2 3.5 3.6
         if (&mStream == &Serial1)
         {
@@ -189,8 +189,7 @@ private:
         }
 #endif
 #else
-        if (TX != RX)
-        {
+        if (TX != RX) {
             pinMode(TX, OUTPUT);
         }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,53 +9,44 @@
 
 #define PIN 25
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, PIN, NEO_GRB + NEO_KHZ400);
-int Pin1= 26;
+int Pin1 = 26;
 float Angle;
 
-DynamixelManager* manager ;
+DynamixelManager *manager;
 
+DynamixelMotor *motor1;
 
-
-DynamixelMotor* motor1;
-//DynamixelMotor* motor2 = manager->createMotor(2 /* ID du XL */, XL430GeneratorFunction); // XL avec l'ID 2
-
-void setup(){
+void setup() {
 // bouton poussoir
 // initialize serial communication:
-Serial.begin(9600);
-manager = new DynamixelManager(2,2); // Manageur qui permet de communiquer avec les XL
-    motor1 = manager->createMotor(1 /* ID du XL */, XL430GeneratorFunction); // XL avec l'ID 1
-pinMode(Pin1, INPUT_PULLUP);
+    Serial.begin(9600);
+    pinMode(Pin1, INPUT_PULLUP);
+
 // Neopixel
-    pinMode(OUTPUT,25);
+    pinMode(OUTPUT, 25);
     strip.begin();
     strip.show(); // Initialise tous les pixels à 'off' (éteint)
+
 // code XL
+    Serial2.begin(57600);
+    manager = new DynamixelManager(4, -1); // Manageur qui permet de communiquer avec les XL
+    //manager = new DynamixelManager(new HardwareSerial(2)); //FIXME: je sais pas laquelle declaration utiliser
+    motor1 = manager->createMotor(1 /* ID du XL */, XL430GeneratorFunction); // XL avec l'ID 1
     motor1->changeLED(true); // allume la LED du XL 1
-//    motor2->changeLED(true); // allume la LED du XL 2
-delay(1000);
-motor1->changeLED(false);
-bool tot;
+    delay(1000);
+    motor1->changeLED(false);
+    bool tot;
     motor1->toggleTorque(false);
-    while(true){
+    while (true) {
         motor1->changeLED(tot);
+        float output;
+        motor1->getCurrentAngle(output);
+        Serial.println(output);
         delay(500);
         tot = !tot;
     }
-// répète à l'infini
-    /*while(true) {
-        motor1->setGoalAngle(90.0f); // fais aller le moteur 1 à 90°
-        //motor2->setGoalAngle(270.0f); // fais aller le moteur 2 à 270°
-        delay(500); // 500ms d'attente
-        motor1->setGoalAngle(180.0f); // fais aller le moteur 1 à 180° (milieu)
-        //motor2->setGoalAngle(180.0f); // fais aller le moteur 2 à 180° (milieu)
-        delay(500); // 500ms d'attente
-        motor1->setGoalAngle(270.0f); // fais aller le moteur 1 à 270°
-        //motor2->setGoalAngle(90.0f); // fais aller le moteur 2 à 90°
-        delay(500); // 500ms d'attente
-    }
-     */
 }
+void loop(){}
 /*
 void led(){
     while (true) {
@@ -85,10 +76,3 @@ void loop(){
 // code XL
 }
 */
-void loop() {
-    boolean recu=motor1->getCurrentAngle(Angle);
-    Serial.print("Bien recu:");
-    Serial.println((recu?"oui":"non"));
-    Serial.println(Angle);
-    delay(500);
-}
