575c4a51dd009aa0d992d478162159fb586c2a14
LamiaLaBg <lamia.salhi@telecom-sudparis.eu>
2020-03-08 01:15:23 +0100

    réorganisation d'initialisation des variables dans les constructeurs et téléchargements de la library SerialSoftware

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 //
 // Created by SALHI Lamia et Tahar et raphi la malice et renax le renard on 07/03/2020.
 //
-
+#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
+//#include <XL430.h>
+//#include "DynamixelManager.h"
 
 void setup(){
 // bouton poussoir
+    // initialize serial communication:
+Serial.begin(9600);
+pinMode(4, INPUT_PULLUP);
 // Neopixel
 // code XL
 }
 void loop(){
-// bouton poussoir
+    // bouton poussoir
+    if (digitalRead (4)== LOW)
+    {
+        Serial.write("Allumée");
+    }
+    else
+    {
+        Serial.write("Éteint");
+    }
 // Neopixel
 // code XL
 }
