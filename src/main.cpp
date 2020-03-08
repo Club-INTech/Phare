70fc3ad1d1cc6f7cdc6ca76052af660dffa0b937
Thrkl <LemKaa@outlook.fr>
2020-03-08 01:34:47 +0100

    code led phare

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,28 @@
 //
 
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#define PIN 25
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, PIN, NEO_GRB + NEO_KHZ400);
 
 void setup(){
 // bouton poussoir
 // Neopixel
+    pinMode(OUTPUT,25);
+    strip.begin();
+    strip.show(); // Initialise tous les pixels à 'off' (éteint)
 // code XL
 }
 void loop(){
 // bouton poussoir
 // Neopixel
+for (int i = 0; i<3; i++) {
+    strip.setPixelColor(i, 255, 255, 255);
+    strip.show();
+    delay(100);
+    strip.setPixelColor(i, 0, 0, 0);
+    strip.show();
+}
+
 // code XL
 }
